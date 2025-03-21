0d95aab08bbd94ea0fe273d8d67f4697cc3592d8
nagasai7772 <130284487+nagasai7772@users.noreply.github.com>
2025-03-21 10:44:54 +0530

    Update Strings.py

--- Problemsolvingsheet/MediumDoc/Python/Strings.py
+++ Problemsolvingsheet/MediumDoc/Python/Strings.py
@@ -1,3 +1,14 @@
+// Write a program that takes a string, string should be of even length. Divide the string into two equals parts, check the number of vowels in both the parts of the string. If both parts of string have same number of vowels then return true otherwise return false.
+
+// Testcase1	:  rebels
+// Output     	:  true
+// Explanation 	:  Given sring rebels divided into two parts, reb and els. In both parts vowels count is same that is 1(e is presented in both the parts) so it returns true.
+
+// Testcase2	:  apples
+// Output     	:  true
+
+// Testcase1	:  mars
+// Output     	:  false
 str_val = 'logicc'
 part1 = ''
 part2 = ''
