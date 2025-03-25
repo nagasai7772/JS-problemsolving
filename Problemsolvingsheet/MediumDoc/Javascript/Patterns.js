1c354f76be4ae18003020b8a8018fcf8056bbbef
nagasai7772 <130284487+nagasai7772@users.noreply.github.com>
2025-03-25 12:51:42 +0530

    Update Patterns.js

--- Problemsolvingsheet/MediumDoc/Javascript/Patterns.js
+++ Problemsolvingsheet/MediumDoc/Javascript/Patterns.js
@@ -64,3 +64,26 @@ for(i=1;i<=rows;i++)
   }
   console.log(s+sum)
 }
+// Write a program that takes number of rows as input and print below respective pattern.
+
+// Testcase1	:  Enter number of rows: 3
+// Output     	: 
+
+//   1
+// 1   2
+// 1  2  3
+
+let rows=3
+for(i=1;i<=rows;i++)
+{
+  s=''
+  for(sp=0;sp<rows-i;sp++)
+  {
+    s+=' '
+  }
+  for(j=1;j<=i;j++)
+  {
+    s+=j+' '
+  }
+  console.log(s)
+}
