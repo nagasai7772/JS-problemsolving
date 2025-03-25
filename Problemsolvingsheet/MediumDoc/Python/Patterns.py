746a41ef4cfb10c1c0a7d7bc1a3eb87a1e51d214
nagasai7772 <130284487+nagasai7772@users.noreply.github.com>
2025-03-25 11:56:36 +0530

    Update Patterns.py

--- Problemsolvingsheet/MediumDoc/Python/Patterns.py
+++ Problemsolvingsheet/MediumDoc/Python/Patterns.py
@@ -1,1 +1,19 @@
 
+
+# Write a program that takes number of rows as input and print below respective pattern.
+
+# Testcase1	:  Enter number of rows: 4
+# Output     	: 
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+
+rows=4
+for i in range(1,rows+1):
+  s=''
+  for j in range(1,i+1):
+    s+=str(j)+' '
+    
+  print(s)
