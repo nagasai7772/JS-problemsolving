542f5f257016232876da8944030cd3379947c323
nagasai7772 <130284487+nagasai7772@users.noreply.github.com>
2025-03-28 00:08:54 +0530

    Update Arrays.js

--- Problemsolvingsheet/MediumDoc/Javascript/Arrays.js
+++ Problemsolvingsheet/MediumDoc/Javascript/Arrays.js
@@ -88,6 +88,123 @@ product=max*secmax
 console.log(max,secmax)
 console.log('The product of two elements in array',product)
 
+// Find the First Duplicate
+
+// Problem: Write a function to return the first duplicate value in an array.
+// Testcase 1:
+// Input: [2, 1, 3, 5, 3, 2]
+// Output: 3
+
+let arr=[2, 1, 3, 5, 3, 2]
+emp=0
+for(i=0;i<arr.length;i++)
+{
+  for(j=i+1;j<arr.length;j++)
+  {
+    if(arr[i]==arr[j])
+    {
+      
+    emp=arr[i]
+    }
+  }
+}
+console.log('The First duplicate in an array',emp)
+// Pair Sum
+
+// Problem: Write a function to find all pairs in an array whose sum is equal to a given target.
+// Testcase 1:
+// Input: [2, 4, 3, 5, 7, 8, 9], 7
+// Output: [[4, 3], [2, 5]]
+
+let arr=[2, 4, 3, 5, 7, 8, 9]
+emp=[]
+target=7
+for(i=0;i<arr.length;i++)
+{
+  for(j=i+1;j<arr.length;j++)
+  {
+    if(arr[i]+arr[j]==target)
+    {
+      
+    emp.push([arr[i],arr[j]])
+    }
+  }
+}
+console.log(emp)
+
+// Move Zeros to End
+
+// Problem: Write a function that moves all zeros in an array to the end while maintaining the order of other elements.
+// Testcase 1:
+// Input: [0, 1, 0, 3, 12]
+// Output: [1, 3, 12, 0, 0]
+
+let arr=[0, 1, 0, 3, 12]
+let res=[]
+for(i=0;i<arr.length;i++)
+{
+  if(arr[i]!=0)
+  {
+    res.push(arr[i])
+  }
+}
+for(j=0;j<arr.length;j++)
+{
+  if(arr[j]<1)
+  {
+    res.push(arr[j])
+  }
+}
+console.log(res)
+
+
+// Write a program that takes a number as input, print the sum of duplicate numbers in the given number.
+
+// Testcase1	:  7473183
+// Output     	:  10
+// Explanation	: In the given number 747383, duplicate digits are 7 and 3 because
+// they occurred more than once in the number. So the sum of 7 and 3 are 10.
+
+let num=7473183
+let str=String(num).split("").map(Number)
+res=0
+emp=[]
+for(i of str)
+{
+  if(!emp.includes(i))
+  {
+    emp.push(i)
+  }
+}
+// console.log(emp)
+for(i of emp)
+{
+  count=0
+  for(j of str)
+  {
+    if(i==j)
+    {
+      count++
+      // console.log(i)
+    }
+  }
+  if(count>1)
+  {
+      res+=i
+    
+  }
+}
+
+console.log(res)
+
+
+
+
+
+
+
+
+
 
 
 
